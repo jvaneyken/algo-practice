dbbbaa55adefc2f34c09007462f94cec9a0b994c
Josh van Eyken <josh.vaneyken@gmail.com>
2024-04-23 18:45:56 -0700

    Add memoized version of fibonacci.js

--- algorithm-practice/recursion/fibonacci.js
+++ algorithm-practice/recursion/fibonacci.js
@@ -1,9 +1,23 @@
+// added memoized version of fibonnaci
+
 const fibonacci = (number) => {
-    if (number === 1 || number === 2) return 1;
+    if (number <= 2) return 1;
     return fibonacci(number - 1) + fibonacci(number - 2);
 }
 
-console.log(fibonacci(10));
-console.log(fibonacci(20));
-console.log(fibonacci(5));
-console.log(fibonacci(40));
+const memoizedFibonacci = (number, memo = {}) => {
+    if (number in memo) return memo[number];
+    if (number <= 2) return 1;
+    memo[number] = memoizedFibonacci(number - 1, memo) + memoizedFibonacci(number - 2, memo);
+    return memo[number];
+}
+
+console.log(fibonacci(10), "fibonacci");
+console.log(fibonacci(20), "fibonacci");
+console.log(fibonacci(5), "fibonacci");
+console.log(fibonacci(40), "fibonacci");
+
+console.log(memoizedFibonacci(10), "memoized fibonacci");
+console.log(memoizedFibonacci(20), "memoized fibonacci");
+console.log(memoizedFibonacci(5), "memoized fibonacci");
+console.log(memoizedFibonacci(40), "memoized fibonacci");
