c093036fa3bad8bfe7a8fbf55f932b6b80b9069d
Josh van Eyken <josh.vaneyken@gmail.com>
2025-02-10 11:06:33 -0800

    Sum.js - recursive sum problem

---
+++ algorithm-practice/recursion/sum.js
@@ -0,0 +1,10 @@
+function sum(numbers) {
+  if (numbers.length === 0) return 0;
+
+  const popped = numbers.shift();
+  return popped + sum(numbers);
+}
+
+console.log(sum([1, 2, 3, 4, 5])); // 15
+console.log(sum([5, 2, 4, 8])); // 19
+console.log(sum([1, 10, 100, 1000])); // 1111
